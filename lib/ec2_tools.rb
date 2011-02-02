709b607dd6a2846920a8e172145670734a52cdd0
Yamada Masaki <masaki@hisme.net>
2011-02-03 00:32:33 +0900

    create Ec2Tools#initialize method

--- lib/ec2_tools.rb
+++ lib/ec2_tools.rb
@@ -1,4 +1,10 @@
-module Ec2Tools
+class Ec2Tools
   autoload :EC2, 'ec2_tools/ec2'
   autoload :Config, 'ec2_tools/config'
+  
+  @config = nil
+  
+  def initialize(config_file = nil)
+    @config = Config.load(config_file)
+  end
 end
--- spec/ec2_tools_spec.rb
+++ spec/ec2_tools_spec.rb
@@ -1,4 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Ec2Tools do
+  describe "#initialize" do
+    it "create config and set as @config" do
+      conf = mock(Ec2Tools::Config)
+      Ec2Tools::Config.should_receive(:load).with(nil) { conf }
+      Ec2Tools.new.instance_variable_get(:@config).should eq(conf)
+    end
+    
+    it "create config from specified file and set as @config" do
+      conf = mock(Ec2Tools::Config)
+      Ec2Tools::Config.should_receive(:load).with('config/hoge.yml') { conf }
+      Ec2Tools.new('config/hoge.yml').
+        instance_variable_get(:@config).should eq(conf)
+    end
+  end
+  
+  # et.keys
+  # et.servers(:key).map(:private_ip)
 end
